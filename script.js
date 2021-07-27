83a01f1a03585937dfa8753ad086052be4c4fb9e
simklim <klimaite.simona@gmail.com>
2021-07-27 23:21:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+function showObjectKeys() {
+  return Object.keys(audi);
+}
+console.log(showObjectKeys());
